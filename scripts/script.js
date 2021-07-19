c696cc74883e9f5aadf647013a9993a7cd13eb54
jimbzy <jimbzy.mail@gmail.com>
2021-07-19 17:28:03 -0500

    Added some javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,34 @@
 const messages = [
-		"Error: Title must be included to post.",
-		"Error: Message must be included to post.",
+		"Error: New Posts must contain a title and message body!",
 		"This is where the new post's title is entered.", 
 		"This is where the new post's message is entered.",
 		"You wrote: ",
 		""
 		];
+		
+var statusBuffer;
 
-function statusUpdate() {
-	alert("Hi");
+function infoHover(msgID) {
+	statusUpdate(msgID);
+}
+
+function statusUpdate(msgID) {
+	document.getElementById("status").innerHTML = messages[msgID];
+}
+
+function clearForm() {
+	document.getElementById("ptitle").value = messages[4];
+	document.getElementById("pbody").value = messages[4];
+}
+
+function validatePost() {
+	var titleStr = document.getElementById("ptitle").value;
+	var postStr = document.getElementById("pbody").value;
+	
+	if ((titleStr == "")||(ptitle == "")) {
+		statusUpdate(0)
+	} else {
+		document.getElementById("status").innerHTML = titleStr + "\n" + postStr;
+	}
+		
 }
