b8228996963f06b204b68a83134a5d4c181a78c7
jimbzy <jimbzy.mail@gmail.com>
2021-07-19 13:21:46 -0500

    Cleaned up directories a bit.

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const messages = [
+		"Error: Title must be included to post.",
+		"Error: Message must be included to post.",
+		"This is where the new post's title is entered.", 
+		"This is where the new post's message is entered.",
+		"You wrote: ",
+		""
+		];
+
+function statusUpdate() {
+	alert("Hi");
+}
